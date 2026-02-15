3e7dd7d15007e77b6c510d579dbbe89fb074efef
Anirudha Bele <beleanirudha8@gmail.com>
2026-02-15 12:48:50 +0530

    refactor(01-tailwind-css): remove redundant font classes and add animated underline effect

--- 01-tailwind-css/src/components/Header.tsx
+++ 01-tailwind-css/src/components/Header.tsx
@@ -3,13 +3,35 @@ const Header = () => {
 		<header className="flex items-center justify-between bg-slate-900 text-white px-8 py-4">
 			<div className="text-3xl font-extrabold">Logo</div>
 			<nav>
-				<li className="flex gap-4 text-xl font-semibold font-bold">
-					<ul className="cursor-pointer hover:underline">Home</ul>
-					<ul className="cursor-pointer hover:underline">About</ul>
-					<ul className="cursor-pointer hover:underline">Contact</ul>
-				</li>
+				<ul className="flex gap-6 text-xl font-bold">
+					<li>
+						<a
+							href="#home"
+							className="cursor-pointer border-b-2 border-transparent transition duration-700 hover:border-white hover:text-slate-300"
+						>
+							Home
+						</a>
+					</li>
+					<li>
+						<a
+							href="#about"
+							className="cursor-pointer border-b-2 border-transparent transition duration-700 hover:border-white hover:text-slate-300"
+						>
+							About
+						</a>
+					</li>
+					<li>
+						<a
+							href="#contact"
+							className="cursor-pointer border-b-2 border-transparent transition duration-700 hover:border-white hover:text-slate-300"
+						>
+							Contact
+						</a>
+					</li>
+				</ul>
 			</nav>
 		</header>
 	);
 };
+
 export default Header;
